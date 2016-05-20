b51d6c563b8cd09cde5e4d5572d5f5318de2b4f2
Ira Lee <ilee2914@gmail.com>
2016-05-20 12:04:59 -0400

    final

--- home.php
+++ home.php
@@ -1,13 +1,13 @@
 <?php
-	echo '<p class="intro">Hello, and welcome to bulletinBoard, the ad section of the web.
+	echo '<p style = "font-family:Ariel, font-size:20pt;" class="intro">Hello, and welcome to SuisseBoard.
 						</br> We\'re here help you find what you\'re looking for, and to help you
 						</br>place ads of your very own. Click on one of the icons below and
-						</br>begin exploring what is currently up on the bulletinBoard.
+						</br>begin exploring what is currently up on the SuisseBoard.
 		</p>
 		<br>
 		<p align="center">
 			<a class="hovertext" href="#" title="Housing"><img class="firstRow" onclick = "loadLeftLite(\'houselite\')" src="png/house.png"/></a>
-			<a class="hovertext" href="#" title="Jobs"><img class="firstRow" onclick = "loadLeftLite(\'jobsLite\')" src="png/tools.png"/></a>
+			<a class="hovertext" href="#" title="Wanted"><img class="firstRow" onclick = "loadLeftLite(\'jobsLite\')" src="png/tools.png"/></a>
 			<a class="hovertext" href="#" title="Shopping"><img class="firstRow" onclick = "loadLeftLite(\'shopplinglite\')" src="png/price-tag.png"/></a>
 			<a class="hovertext" href="#" title="Community"><img class="firstRow" onclick = "loadLeftLite(\'communitiesLite\')" src="png/village.png"/></a>
 
@@ -16,7 +16,7 @@
 		<p align="center">
 			<a class="hovertext1" href="#" title="Group Activites"><img class="secondRow" src="png/sports.png"/></a>
 			<a class="hovertext1" href="#" title="Forums"><img class="secondRow" src="png/speech-bubbles.png"/></a>
-			<a class="hovertext1" href="#" title="Services"><img class="secondRow" src="png/technology.png"/></a>
+			<a class="hovertext1" href="#" title="Servies"><img class="secondRow" src="png/technology.png"/></a>
 			<a class="hovertext1" href="#" title="Gigs"><img class="secondRow" src="png/laptop.png"/></a>
 
 		</p>';
