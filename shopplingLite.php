134ad166255c2808ddb1a221fa6e8836c413bc3e
KevSan <kevinsanchez1989@gmail.com>
2016-05-19 20:03:04 -0400

    Removed Back Ground Image
    The Shopping Lite Box still had the old back ground. Updated it.

--- shopplingLite.php
+++ shopplingLite.php
@@ -4,7 +4,6 @@ echo '
 	
 	<h1 align="center" style="padding:0; color:black;">Shopping</h1>
 	
-	<center><img class="boards" src="png/BulletinBoard1.png" height="700px" width="400px"/></center>
 	
 	<div style="position: absolute; top: 200px; left: 130px; width: 400px; font-weight: bold; color: white;">
 	<p class="textPointer" onclick="send(\'register\')" ><font size="6">vehichles</font></p>
