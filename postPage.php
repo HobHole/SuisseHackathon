4d37b013fd93a1eb83ec55b601a3150c19b9e973
Ira Lee <ilee2914@gmail.com>
2016-05-20 03:40:47 -0400

    m

--- postPage.php
+++ postPage.php
@@ -9,7 +9,7 @@
 	$cmd = "SELECT subcatid FROM subcategory WHERE name = '".$subCat."';";
 	$return = mysqli_query($db, $cmd);
 	$returnAss = mysqli_fetch_assoc($return);
-	$subcatid = $returnAss['id'];
+	$subcatid = $returnAss['subcatid'];
 	
 	$cmd = "SELECT * FROM posts WHERE subcatid = '".$subcatid."' AND active = 1 ORDER BY postid;";
 	$return = mysqli_query($db, $cmd);
@@ -20,36 +20,55 @@
 	}
 	
 	echo '
-			<h1 align="center"; style="color:black;">'.$subcat.'</h1>
+			<h1 align="center"; style="color:black;">'.$subCat.'</h1>
 		
 		';
 	
-	for ($i = 0; $i < 2; $i++) {
+	for ($i = 0; $i < count($resultSet); $i++) {
 		$creatorid = $resultSet[$i]['creatorid'];
 		$title = $resultSet[$i]['title'];
 		$description = $resultSet[$i]['description'];
 		$locationid = $resultSet[$i]['location'];
 		$price = $resultSet[$i]['price'];
+		$image1 = $resultSet[$i]['image1'];
+		$image2 = $resultSet[$i]['image2'];
+		$image3 = $resultSet[$i]['image3'];
 		
 		$cmd = "SELECT name FROM users WHERE id = '".$creatorid."';";
 		$return = mysqli_query($db, $cmd);
 		$returnAss = mysqli_fetch_assoc($return);
 		$name = $returnAss['name'];
 		
-		$cmd = "SELECT name FROM locations WHERE locationid = '".$location."';";
+		$cmd = "SELECT name FROM locations WHERE locationid = '".$locationid."';";
 		$return = mysqli_query($db, $cmd);
 		$returnAss = mysqli_fetch_assoc($return);
 		$location = $returnAss['name'];
 				
 		echo '
 		
-			<div style = "width:50%; height: 80px;" align="center" id="mailSlots">
-				<span style = font-weight:8; font-size:14pt;">'.$subject.' : '.$title.'</span>
+			<div style = "width:50%; height: 300px;" align="center" id="mailSlots">
+				<span style = font-weight:8; font-size:14pt;">'.$name.' : '.$title.'</span>
 				<br>
 				<span style = "font-size:12pt;">Location : '.$location.'    Price : $'.$price.'</span>
+				</br>
 				<span>'.$description.'</span>
 				<br>
 				<br>
+				';
+			$dir = "uploads/";
+			$filename = $dir.$image1;
+			if (file_exists($filename)) {
+				echo '<img style = "height:100px; width:100px;" src = "'.$filename.'"/>';
+			}
+			$filename = $dir.$image2;
+			if (file_exists($filename)) {
+				echo '<img  style = "height:100px; width:100px;"src = "'.$filename.'"/>';
+			}
+			$filename = $dir.$image3;
+			if (file_exists($filename)) {
+				echo '<img  style = "height:100px; width:100px;"src = "'.$filename.'"/>';
+			}
+			echo'
 			</div>
 		';
 	}
